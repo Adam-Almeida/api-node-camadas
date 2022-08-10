041ff778b48d11505a5752811217e2c182fcf474
adam-almeida <adamportifolio@gmail.com>
2022-08-10 11:46:35 -0300

    Finalizando as camadas

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,8 +1,22 @@
-
+import { v4 } from 'uuid'
 
 
 class UserRepository {
-    
+
+    users = []
+
+    findByUsername(username) {
+        const userExists = this.users.find(user => user.username === username)
+        return userExists
+    }
+
+    save(user){
+        const id = new v4()
+        const userWithId = {...user, id }
+        this.users.push(userWithId)
+
+        return user
+    }
 }
 
 export { UserRepository }
--- src/services/create.user.service.js
+++ src/services/create.user.service.js
@@ -1,9 +1,20 @@
-
+import { UserRepository } from "../repositories/user.repository";
 
 class CreateUserService {
-    async execute(data){
-        
+  execute(data) {
+    if (!!data.username) {
+      throw new Error("Preencha o Username");
     }
+
+    const userRepository = new UserRepository();
+    const userExists = userRepository.findByUsername(data.username);
+
+    if (userExists) {
+      throw new Error("O usuário já existe no banco de dados");
+    }
+
+    return userRepository.save(data);
+  }
 }
 
-export { CreateUserService }
+export { CreateUserService };
